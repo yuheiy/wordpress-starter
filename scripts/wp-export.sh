a3496f8009e8bd146aee8b8b0bde190beec5899a
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2020-08-20 17:55:40 +0900

    rename scripts/fixtures to scripts/snapshot

--- scripts/wp-export.sh
+++ scripts/wp-export.sh
@@ -7,11 +7,11 @@ SQL_FILE="wordpress.sql"
 script_dir="$(cd $(dirname "${BASH_SOURCE[0]}"); pwd)"
 container_id="$(docker ps -f name=_wordpress_ -q)"
 
-rm -rf "$script_dir/fixtures"
-mkdir -p "$script_dir/fixtures"
+rm -r "$script_dir/snapshot"
+mkdir "$script_dir/snapshot"
 
 wp-env run cli "rm -f $SQL_FILE"
 wp-env run cli "wp db export $SQL_FILE"
-docker cp "$container_id:/var/www/html/$SQL_FILE" "$script_dir/fixtures/."
+docker cp "$container_id:/var/www/html/$SQL_FILE" "$script_dir/snapshot/."
 
-docker cp "$container_id:/var/www/html/wp-content/uploads" "$script_dir/fixtures/."
+docker cp "$container_id:/var/www/html/wp-content/uploads" "$script_dir/snapshot/."
--- scripts/wp-import.sh
+++ scripts/wp-import.sh
@@ -7,10 +7,10 @@ SQL_FILE="wordpress.sql"
 script_dir="$(cd $(dirname "${BASH_SOURCE[0]}"); pwd)"
 container_id="$(docker ps -f name=_wordpress_ -q)"
 
-docker cp "$script_dir/fixtures/$SQL_FILE" "$container_id:/var/www/html/$SQL_FILE"
+docker cp "$script_dir/snapshot/$SQL_FILE" "$container_id:/var/www/html/$SQL_FILE"
 wp-env run cli "wp db import $SQL_FILE"
 
 wp-env run cli "wp language core install ja"
 
 wp-env run cli "rm -rf wp-content/uploads"
-docker cp "$script_dir/fixtures/uploads" "$container_id:/var/www/html/wp-content/."
+docker cp "$script_dir/snapshot/uploads" "$container_id:/var/www/html/wp-content/."
