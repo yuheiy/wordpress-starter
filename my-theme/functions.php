d9a43136d7e8bf95b211b9c8ef3c4beda6729d81
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2020-08-26 14:30:06 +0900

    output some default tags

--- my-theme/functions.php
+++ my-theme/functions.php
@@ -4,13 +4,10 @@ require __DIR__ . '/inc/news.php';
 require __DIR__ . '/inc/head.php';
 require __DIR__ . '/inc/admin.php';
 
-remove_action('wp_head', 'rest_output_link_wp_head');
-remove_action('template_redirect', 'rest_output_link_header', 11);
 remove_action('wp_head', 'feed_links_extra', 3);
 remove_action('wp_head', 'rsd_link');
 remove_action('wp_head', 'wlwmanifest_link');
 remove_action('wp_head', 'print_emoji_detection_script', 7);
-remove_action('wp_head', 'wp_generator');
 remove_action('wp_print_styles', 'print_emoji_styles');
 add_filter('emoji_svg_url', '__return_false');
 remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0);
