6b131ed84fcb657c0522874b904014be6a7211f0
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2020-11-07 18:11:52 +0900

    use kebab-case for filenames

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,7 @@ module.exports = {
   },
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|ico)$":
-      "<rootDir>/resources/assets/test/__mocks__/fileMock.js",
+      "<rootDir>/resources/assets/test/__mocks__/file-mock.js",
   },
   setupFiles: ["<rootDir>/resources/assets/test/setup.ts"],
 };
