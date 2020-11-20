bdbe0a2ddbeee55065223a13b80f0f0c97b9b870
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2020-11-20 15:15:23 +0900

    don’t reinstall if ACF is already installed
    don’t reinstall if ACF is already installed

--- scripts/install-acf-pro.sh
+++ scripts/install-acf-pro.sh
@@ -3,8 +3,17 @@
 set -eu
 
 root_dir="$(dirname "$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)")"
+plugin_dir="$root_dir/plugins"
+acf_plugin_dir="$plugin_dir/advanced-custom-fields-pro"
 
-rm -rf "$root_dir/plugins/advanced-custom-fields-pro"
-curl "https://connect.advancedcustomfields.com/v2/plugins/download?p=pro&k=SET_YOUR_KEY" > "$root_dir/advanced-custom-fields-pro.zip"
-unzip "$root_dir/advanced-custom-fields-pro.zip" -d "$root_dir/plugins"
-rm "$root_dir/advanced-custom-fields-pro.zip"
+if [ -e $acf_plugin_dir ]; then
+  echo "Advanced Custom Fields PRO is already installed"
+  exit 0
+fi
+
+acf_zip_file="$root_dir/advanced-custom-fields-pro.zip"
+
+rm -rf $acf_plugin_dir
+curl "https://connect.advancedcustomfields.com/v2/plugins/download?p=pro&k=SET_YOUR_KEY" > $acf_zip_file
+unzip $acf_zip_file -d $plugin_dir
+rm $acf_zip_file
