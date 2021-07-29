a1ef6d68735fd5961c10a2305c2bf7a465b35230
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2021-07-30 05:02:54 +0900

    reorganize block styles

--- drygen.config.js
+++ drygen.config.js
@@ -3,19 +3,32 @@
  */
 module.exports = {
 	rules: [
-		...["blocks", "components"].map((type) => ({
-			name: `scss/${type}`,
+		...["style", "editor"].map((type) => ({
+			name: `scss/blocks/${type}`,
 			dependencies: [
-				`mytheme/assets/${type}/**/*.scss`,
-				`!mytheme/assets/${type}/${type}.scss`,
+				`mytheme/assets/blocks/**/${type}.scss`,
+				`!mytheme/assets/blocks/${type}.scss`,
 			],
 			outputs: [
 				{
-					path: `mytheme/assets/${type}/${type}.scss`,
-					template: "mytheme/assets/styles/import.scss.ejs",
+					path: `mytheme/assets/blocks/${type}.scss`,
+					template: "mytheme/assets/blocks/import.scss.ejs",
 				},
 			],
 		})),
+		{
+			name: "scss/components",
+			dependencies: [
+				"mytheme/assets/components/**/*.scss",
+				"!mytheme/assets/components/components.scss",
+			],
+			outputs: [
+				{
+					path: "mytheme/assets/components/components.scss",
+					template: "mytheme/assets/styles/import.scss.ejs",
+				},
+			],
+		},
 		...["settings", "tools", "objects", "scopes", "themes"].map((type) => ({
 			name: `scss/${type}`,
 			dependencies: [
