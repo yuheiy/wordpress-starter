6afb4502d5b2e9e0465b06b74aeb4e6ced633227
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2021-08-10 02:34:29 +0900

    install rollup-plugin-visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
+import { visualizer } from "rollup-plugin-visualizer";
 import { defineConfig } from "vite";
 import liveReload from "vite-plugin-live-reload";
 
 export default defineConfig({
-	plugins: [liveReload("mytheme/**/*.{php,twig}")],
+	plugins: [liveReload("mytheme/**/*.{php,twig}"), visualizer()],
 	logLevel: "warn",
 	build: {
 		outDir: "mytheme/assets/build",
