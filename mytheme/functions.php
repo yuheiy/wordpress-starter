bbffce099653a353caaa750e8fcb87790ce8d139
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2021-10-02 11:45:07 +0900

    copy ogp implementation from wd_s

--- mytheme/functions.php
+++ mytheme/functions.php
@@ -102,37 +102,153 @@ if (wp_get_environment_type() === "local" && SCRIPT_DEBUG) {
 	});
 }
 
+// https://github.com/WebDevStudios/wd_s/blob/3c94e66861e12632d47a7450c9011dc60e62caff/inc/hooks.php#L228-L358
 add_action("wp_head", function () {
-	if (!($title = trim(wp_title("", false)))) {
-		$title = get_bloginfo("name");
+	// Set a post global on single posts. This avoids grabbing content from the first post on an archive page.
+	if (is_singular()) {
+		global $post;
 	}
 
-	$description = get_bloginfo("description");
+	// Get the post content.
+	$post_content = !empty($post) ? $post->post_content : "";
 
-	if (is_single()) {
-		$type = "archive";
-	} else {
-		$type = "website";
-	}
+	// Strip all tags from the post content we just grabbed.
+	$default_content = $post_content
+		? wp_strip_all_tags(strip_shortcodes($post_content))
+		: $post_content;
 
-	$image = get_theme_file_uri("assets/images/ogp.png");
+	// Set our default title.
+	$default_title = get_bloginfo("name");
 
-	$site_name = get_bloginfo("name");
+	// Set our default URL.
+	$default_url = get_permalink();
+
+	// Set our base description.
+	$default_base_description = get_bloginfo("description")
+		? get_bloginfo("description")
+		: esc_html__("Visit our website to learn more.", "_s");
+
+	// Set the card type.
+	$default_type = "article";
+
+	// Get our custom logo URL. We'll use this on archives and when no featured image is found.
+	$logo_id = get_theme_mod("custom_logo");
+	$logo_image = $logo_id ? wp_get_attachment_image_src($logo_id, "full") : "";
+	$logo_url = $logo_id ? $logo_image[0] : "";
+
+	// Set our final defaults.
+	$card_title = $default_title;
+	$card_description = $default_base_description;
+	$card_long_description = $default_base_description;
+	$card_url = $default_url;
+	$card_image = $logo_url;
+	$card_type = $default_type;
 
 	$locale = get_locale();
 
-	$url = Timber\URLHelper::get_current_url();
+	// Let's start overriding!
+	// All singles.
+	if (is_singular()) {
+		if (has_post_thumbnail()) {
+			$card_image = get_the_post_thumbnail_url();
+		}
+	}
 
-	$twitter_card = "summary_large_image";
+	// Single posts/pages that aren't the front page.
+	if (is_singular() && !is_front_page()) {
+		$card_title = get_the_title() . " - " . $default_title;
+		$card_description = $default_content
+			? wp_trim_words($default_content, 53, "...")
+			: $default_base_description;
+		$card_long_description = $default_content
+			? wp_trim_words($default_content, 140, "...")
+			: $default_base_description;
+	}
+
+	// Categories, Tags, and Custom Taxonomies.
+	if (is_category() || is_tag() || is_tax()) {
+		$term_name = single_term_title("", false);
+		$card_title = $term_name . " - " . $default_title;
+		$specify = is_category()
+			? esc_html__("categorized in", "_s")
+			: esc_html__("tagged with", "_s");
+		$queried_object = get_queried_object();
+		$card_url = get_term_link($queried_object);
+		$card_type = "website";
+
+		// Translators: get the term name.
+		$card_long_description = sprintf(
+			esc_html__('Posts %1$s %2$s.', "_s"),
+			$specify,
+			$term_name
+		);
+		$card_description = $card_long_description;
+	}
+
+	// Search results.
+	if (is_search()) {
+		$search_term = get_search_query();
+		$card_title = $search_term . " - " . $default_title;
+		$card_url = get_search_link($search_term);
+		$card_type = "website";
+
+		// Translators: get the search term.
+		$card_long_description = sprintf(
+			esc_html__("Search results for %s.", "_s"),
+			$search_term
+		);
+		$card_description = $card_long_description;
+	}
+
+	if (is_home()) {
+		$posts_page = get_option("page_for_posts");
+		$card_title = get_the_title($posts_page) . " - " . $default_title;
+		$card_url = get_permalink($posts_page);
+		$card_type = "website";
+	}
+
+	// Front page.
+	if (is_front_page()) {
+		$front_page = get_option("page_on_front");
+		$card_title = $front_page
+			? get_the_title($front_page) . " - " . $default_title
+			: $default_title;
+		$card_url = get_home_url();
+		$card_type = "website";
+	}
+
+	// Post type archives.
+	if (is_post_type_archive()) {
+		$post_type_name = get_post_type();
+		$card_title = $post_type_name . " - " . $default_title;
+		$card_url = get_post_type_archive_link($post_type_name);
+		$card_type = "website";
+	}
+
+	// Media page.
+	if (is_attachment()) {
+		$attachment_id = get_the_ID();
+		$card_image = wp_attachment_is_image($attachment_id)
+			? wp_get_attachment_image_url($attachment_id, "full")
+			: $card_image;
+	}
 	?>
-	<meta name="description" content="<?= esc_attr($description) ?>">
-	<meta name="twitter:card" content="<?= esc_attr($twitter_card) ?>">
-	<meta property="og:title" content="<?= esc_attr($title) ?>">
-	<meta property="og:type" content="<?= esc_attr($type) ?>">
-	<meta property="og:image" content="<?= esc_url($image) ?>">
-	<meta property="og:url" content="<?= esc_url($url) ?>">
-	<meta property="og:description" content="<?= esc_attr($description) ?>">
-	<meta property="og:site_name" content="<?= esc_attr($site_name) ?>">
+	<meta name="description" content="<?php echo esc_attr(
+ 	$card_long_description
+ ); ?>" />
+ 	<meta name="twitter:card" content="summary_large_image">
+	<meta property="og:title" content="<?php echo esc_attr($card_title); ?>" />
+	<meta property="og:description" content="<?php echo esc_attr(
+ 	$card_description
+ ); ?>" />
+	<meta property="og:url" content="<?php echo esc_url($card_url); ?>" />
+	<?php if ($card_image): ?>
+		<meta property="og:image" content="<?php echo esc_url($card_image); ?>" />
+	<?php endif; ?>
+	<meta property="og:site_name" content="<?php echo esc_attr(
+ 	$default_title
+ ); ?>" />
+	<meta property="og:type" content="<?php echo esc_attr($card_type); ?>" />
 	<meta property="og:locale" content="<?= esc_attr($locale) ?>">
 	<?php
 });
