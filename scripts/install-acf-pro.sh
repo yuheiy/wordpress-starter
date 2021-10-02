4306bf3b2d54c612187879d3d38356fd99da42fa
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2021-10-02 11:19:07 +0900

    refactor install-acf-pro.sh

--- scripts/install-acf-pro.sh
+++ scripts/install-acf-pro.sh
@@ -6,20 +6,20 @@ if [ -n "$CI" ]; then
   exit 0
 fi
 
-root_dir="$(dirname "$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)")"
-plugin_dir="$root_dir/plugins"
+cd "$(dirname "$(dirname "${BASH_SOURCE[0]}")")"
+
+plugin_dir="plugins"
 acf_plugin_dir="$plugin_dir/advanced-custom-fields-pro"
+acf_zip_file="advanced-custom-fields-pro.zip"
 
 if [ -e "$acf_plugin_dir" ]; then
-	echo "Advanced Custom Fields PRO is already installed"
-	exit 0
+  echo "Advanced Custom Fields PRO is already installed"
+  exit 0
 fi
 
-source "$root_dir/.env"
+# shellcheck disable=SC1091
+source .env
 
-acf_zip_file="$root_dir/advanced-custom-fields-pro.zip"
-
-rm -rf "$acf_plugin_dir"
-curl "https://connect.advancedcustomfields.com/v2/plugins/download?p=pro&k=$ACF_KEY" > "$acf_zip_file"
+curl "https://connect.advancedcustomfields.com/v2/plugins/download?p=pro&k=$ACF_KEY" >"$acf_zip_file"
 unzip "$acf_zip_file" -d "$plugin_dir"
 rm "$acf_zip_file"
