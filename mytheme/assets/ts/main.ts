a09781efdaf1fe7227f3292677ec373ca66124b0
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2021-10-13 07:57:20 +0900

    remove legacy comment

--- mytheme/assets/ts/main.ts
+++ mytheme/assets/ts/main.ts
@@ -1,6 +1,5 @@
 import "../scss/main.scss";
 
-// https://vitejs.dev/guide/env-and-mode.html#env-variables
 if (process.env.NODE_ENV === "development") {
 	console.log({ NODE_ENV: process.env.NODE_ENV });
 }
