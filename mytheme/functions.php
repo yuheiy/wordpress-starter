6a4cd2207f5edaacbc6c19cad03429f8003c0294
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2022-01-27 19:21:51 +0900

    use separated `editor-style.css`

--- mytheme/functions.php
+++ mytheme/functions.php
@@ -33,7 +33,7 @@ add_action("after_setup_theme", function () {
 	add_theme_support("align-wide");
 
 	add_theme_support("editor-styles");
-	add_editor_style("build/index.css");
+	add_editor_style();
 
 	add_theme_support("responsive-embeds");
 });
