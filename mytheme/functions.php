1347bd59b16f11bfd4155c5d7d8f062ee60933b5
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2022-01-27 18:15:59 +0900

    add editor style

--- mytheme/functions.php
+++ mytheme/functions.php
@@ -33,6 +33,9 @@ add_action("after_setup_theme", function () {
 
 	add_theme_support("align-wide");
 
+	add_theme_support("editor-styles");
+	add_editor_style("build/index.css");
+
 	add_theme_support("responsive-embeds");
 });
 
