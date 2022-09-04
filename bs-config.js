146af0f2769713ed251d8e12cc4988b1ead4b5d3
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2022-09-04 18:00:21 +0900

    remove ignore options in bs config

--- bs-config.js
+++ bs-config.js
@@ -4,7 +4,6 @@ module.exports = {
 		"source/wp-content/themes/mytheme/build",
 		"source/wp-content/themes/mytheme/**/*.{php,twig}",
 	],
-	ignore: ["node_modules"],
 	proxy: "http://localhost:8888",
 	ghostMode: false,
 	open: false,
