ddd6b4d1c78d4fae93074f4b7ea71ac4971794e1
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2022-09-17 19:57:37 +0900

    fix quote handling in setup.sh

--- env/setup.sh
+++ env/setup.sh
@@ -10,20 +10,20 @@ wp theme activate mytheme
 wp rewrite structure "/%postname%/"
 wp rewrite flush --hard
 
-rm -rf "${root}/wp-content/uploads/*/"
-cp -r "${root}/env/uploads/." "${root}/wp-content/uploads/"
+rm -rf "${root}"/wp-content/uploads/*/
+cp -r "${root}"/env/uploads/. "${root}"/wp-content/uploads/
 
 # rewrite localhost to internal IP address because localhost causes cURL error 7
-sed -e "s/<wp:attachment_url>http:\/\/localhost:8888\//<wp:attachment_url>http:\/\/${ip_address}:8888\//g" "${root}/env/data.xml" >"${root}/env/data.edited.xml"
-wp import "${root}/env/data.edited.xml" --authors=create
-rm "${root}/env/data.edited.xml"
+sed -e "s/<wp:attachment_url>http:\/\/localhost:8888\//<wp:attachment_url>http:\/\/${ip_address}:8888\//g" "${root}"/env/data.xml >"${root}"/env/data.edited.xml
+wp import "${root}"/env/data.edited.xml --authors=create
+rm "${root}"/env/data.edited.xml
 
 wp option update blogname "wordpress-starter"
 wp option update blogdescription "WordPressテーマ構築のための開発環境"
 wp option update timezone_string "Asia/Tokyo"
 wp option update date_format "Y年n月j日"
 wp option update time_format "H:i"
-wp option update site_icon 9
+wp option update site_icon "9"
 
 wp option patch update wpseo content_analysis_active false --format=json
 wp option patch update wpseo keyword_analysis_active false --format=json
@@ -34,4 +34,4 @@ wp option patch update wpseo enable_metabox_insights false --format=json
 wp option patch update wpseo enable_link_suggestions false --format=json
 
 wp option patch update wpseo_social og_default_image "http://localhost:8888/wp-content/uploads/2022/09/ogp.png"
-wp option patch update wpseo_social og_default_image_id 11
+wp option patch update wpseo_social og_default_image_id "11"
