c375cebc998ed86c3ba6d7c5838dfa99acfae4fd
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2022-09-20 11:35:52 +0900

    refactor blocks.php

--- source/wp-content/themes/mytheme/inc/blocks.php
+++ source/wp-content/themes/mytheme/inc/blocks.php
@@ -6,9 +6,9 @@ use Timber;
 
 add_action("init", function () {
 	$json = file_get_contents(dirname(__DIR__) . "/data/lzb-export-blocks.json");
-	$export_blocks = json_decode($json, true);
+	$exported_blocks = json_decode($json, true);
 
-	foreach ($export_blocks as $block) {
+	foreach ($exported_blocks as $block) {
 		lazyblocks()->add_block($block);
 	}
 });
@@ -23,25 +23,32 @@ add_filter(
 );
 
 foreach (glob(dirname(__DIR__) . "/views/blocks/lazyblock/*", GLOB_ONLYDIR) as $dir) {
-	$block = basename($dir);
+	$block_name = basename($dir);
 
 	add_filter(
-		"lazyblock/$block/frontend_callback",
+		"lazyblock/$block_name/frontend_callback",
 		__NAMESPACE__ . "\\frontend_block_output",
 		10,
 		2
 	);
-	add_filter("lazyblock/$block/editor_callback", __NAMESPACE__ . "\\editor_block_output", 10, 2);
+	add_filter(
+		"lazyblock/$block_name/editor_callback",
+		__NAMESPACE__ . "\\editor_block_output",
+		10,
+		2
+	);
 }
 
 function frontend_block_output($output, $attributes)
 {
 	ob_start();
 
+	$block_name = $attributes["lazyblock"]["slug"];
+
 	$context = Timber::context();
 	$context["attributes"] = $attributes;
 
-	Timber::render("blocks/" . $attributes["lazyblock"]["slug"] . "/block.twig", $context);
+	Timber::render("blocks/$block_name/block.twig", $context);
 
 	return ob_get_clean();
 }
@@ -50,10 +57,12 @@ function editor_block_output($output, $attributes)
 {
 	ob_start();
 
+	$block_name = $attributes["lazyblock"]["slug"];
+
 	$context = Timber::context();
 	$context["attributes"] = $attributes;
 
-	Timber::render("blocks/" . $attributes["lazyblock"]["slug"] . "/editor.twig", $context);
+	Timber::render("blocks/$block_name/block.twig", $context);
 
 	return ob_get_clean();
 }
