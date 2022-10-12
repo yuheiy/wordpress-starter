05a0797c3accc2b8a0754af692dca06e9d8cc712
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2022-10-12 13:29:19 +0900

    refactor export.sh

--- env/export.sh
+++ env/export.sh
@@ -6,5 +6,5 @@ root=$(dirname "$(wp config path)")
 
 wp export --dir="${root}/env" --filename_format="data.xml"
 
-rm -rf "${root}/env/uploads"
+rm -rf "${root}/env/uploads/"
 cp -r "${root}/wp-content/uploads/." "${root}/env/uploads/"
