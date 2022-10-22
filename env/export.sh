7eff92a76a1678d232a9d49435caaedf3867fc53
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2022-10-22 16:29:41 +0900

    refine export.sh/setup.sh

--- env/export.sh
+++ env/export.sh
@@ -6,13 +6,13 @@ root=$(dirname "$(wp config path)")
 
 rm -rf "${root}"/env/wordpress.*.xml
 
-wp export --dir="${root}/env" --post_type=post --filename_format="wordpress.post.xml"
-wp export --dir="${root}/env" --post_type=page --filename_format="wordpress.page.xml"
-wp export --dir="${root}/env" --post_type=attachment --filename_format="wordpress.attachment.xml"
+wp export --dir="${root}/env/" --post_type=post --filename_format=wordpress.post.xml
+wp export --dir="${root}/env/" --post_type=page --filename_format=wordpress.page.xml
+wp export --dir="${root}/env/" --post_type=attachment --filename_format=wordpress.attachment.xml
 
 wp post-type list --_builtin=0 --can_export=1 --format=csv | tail +2 | while read -r line; do
   post_type="$(echo "${line}" | cut -d "," -f 1)"
-  wp export --dir="${root}/env" --post_type="${post_type}" --filename_format="wordpress.${post_type}.xml"
+  wp export --dir="${root}/env/" --post_type="${post_type}" --filename_format="wordpress.${post_type}.xml"
 done
 
 rm -rf "${root}/env/uploads/"
--- env/setup.sh
+++ env/setup.sh
@@ -9,35 +9,37 @@ wp site switch-language ja
 
 wp theme activate mytheme
 
-wp rewrite structure "/%postname%/"
+wp rewrite structure /%postname%/
 wp rewrite flush --hard
 
 wp post delete $(wp post list --post_type=any --format=ids) --force
 
-rm -rf "${root}/wp-content/uploads/"*
+rm -rf "${root}"/wp-content/uploads/*
 cp -r "${root}/env/uploads/." "${root}/wp-content/uploads/"
 
-find "${root}/env" -name "wordpress.*.xml" | while read -r pathname; do
+find "${root}/env/" -maxdepth 1 -name "wordpress.*.xml" | while read -r pathname; do
   # rewrite localhost to internal IP address because localhost causes cURL error 7
   sed -e "s/<wp:attachment_url>http:\/\/localhost:8888\//<wp:attachment_url>http:\/\/${ip_address}:8888\//g" "${pathname}" >"${pathname}.edited"
   wp import "${pathname}.edited" --authors=create
   rm "${pathname}.edited"
 done
 
-wp option update blogname "wordpress-starter"
-wp option update blogdescription "WordPressテーマ構築のための開発環境"
-wp option update timezone_string "Asia/Tokyo"
-wp option update date_format "Y年n月j日"
-wp option update time_format "H:i"
+wp option update blogname wordpress-starter
+wp option update blogdescription WordPressテーマ構築のための開発環境
+wp option update timezone_string Asia/Tokyo
+wp option update date_format Y年n月j日
+wp option update time_format H:i
 wp option update site_icon 5
 
-wp option patch update wpseo content_analysis_active false --format=json
-wp option patch update wpseo keyword_analysis_active false --format=json
-wp option patch update wpseo enable_admin_bar_menu false --format=json
-wp option patch update wpseo enable_cornerstone_content false --format=json
-wp option patch update wpseo enable_text_link_counter false --format=json
-wp option patch update wpseo enable_metabox_insights false --format=json
-wp option patch update wpseo enable_link_suggestions false --format=json
+wp option patch update wpseo disableadvanced_meta 0
+wp option patch update wpseo content_analysis_active 0
+wp option patch update wpseo keyword_analysis_active 0
+wp option patch update wpseo enable_admin_bar_menu 0
+wp option patch update wpseo enable_cornerstone_content 0
+wp option patch update wpseo enable_text_link_counter 0
+wp option patch update wpseo enable_metabox_insights 0
+wp option patch update wpseo enable_enhanced_slack_sharing 0
+wp option patch update wpseo dismiss_configuration_workout_notice 1
 
-wp option patch update wpseo_social og_default_image "http://localhost:8888/wp-content/uploads/2022/10/ogp.png"
+wp option patch update wpseo_social og_default_image http://localhost:8888/wp-content/uploads/2022/10/ogp.png
 wp option patch update wpseo_social og_default_image_id 15
