2b978cf3a1df1610a58c9f2400873032a90a6a86
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2022-10-24 22:46:51 +0900

    improve components/stores plugins

--- source/wp-content/themes/mytheme/src/scripts/stores/index.ts
+++ source/wp-content/themes/mytheme/src/scripts/stores/index.ts
@@ -7,8 +7,8 @@ export default function (Alpine: Alpine) {
 	for (const key of context.keys()) {
 		if (key === "./index.ts") continue;
 
-		const [base] = key.split("/").reverse();
-		const [name] = base.split(".");
+		const base = key.split("/").at(-1);
+		const name = base.split(".").slice(0, -1).join(".");
 		const module = context(key);
 		Alpine.store(camelCase(name), module.default());
 	}
